820f6811d97b27f2572df87b58810adf57871a6c
realtee01 <tobilobaakala@gmail.com>
2026-03-22 19:08:24 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,12 @@ function App() {
       <Router>
         <div className="min-h-screen bg-[#0a0a0a] text-white font-body selection:bg-terra/30">
           <Navbar />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/movie/:id" element={<MovieDetail />} />
-              <Route path="/watchlist" element={<Watchlist />} />
-            </Routes>
+          <<Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/movie/:id" element={<MovieDetail />} />
+  <Route path="/watchlist" element={<Watchlist />} />
+</Routes>
+
           </main>
         </div>
       </Router>
