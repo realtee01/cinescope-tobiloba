d1e6459e8c9958a4e93d99dd07424ed759f0bfc2
realtee01 <tobilobaakala@gmail.com>
2026-03-22 13:31:36 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { MovieProvider } from './context/MovieContext';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import MovieDetail from './pages/MovieDetail';
+import Watchlist from './pages/Watchlist';
+
+function App() {
+  return (
+    <MovieProvider>
+      <Router>
+        <div className="min-h-screen bg-[#0a0a0a] text-white font-body selection:bg-terra/30">
+          <Navbar />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/movie/:id" element={<MovieDetail />} />
+              <Route path="/watchlist" element={<Watchlist />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </MovieProvider>
+  );
+}
+
+export default App;
